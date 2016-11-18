3baecd1f5a1de79fbf4b8f3212155666d6570716
sebastian correa <correa.m.sebastian@gmail.com>
2016-11-18 02:00:08 -0500

    scan ready

--- main.ts
+++ main.ts
@@ -5,8 +5,8 @@ let source = Observable.from(numbers);
 interface Istamp {
   color: string
   shape: string
-  index:number
-  secuence?:string
+  index: number
+  secuence?: string
 }
 class MyObserver implements Observer<Istamp>{
 
@@ -29,18 +29,18 @@ class MyObserver implements Observer<Istamp>{
 
 class ScmObservable {
   private observable: Observable<Istamp>;
-  private index:number=0;
-  private secuence:string=' ';
+  private index: number = 1;
+  private secuence: string = ' ';
 
   constructor(private geometry: string = '.', private color: string = 'blue', private limit: number = 10, private interval: number = 1000) {
     console.log('constructor');
 
     this.observable = Observable.create(this.link)
-    .do((stamp:Istamp)=>{
-      // console.log(`%c${stamp.shape}(${stamp.index})`, `color: ${stamp.color}; font-size:30px;`);
-      // console.log(stamp.index);
-      console.log(`%c${stamp.index}${stamp.secuence}${stamp.shape}`, `color: ${stamp.color}; font-size:15px;`);
-    })
+      .do((stamp: Istamp) => {
+        // console.log(`%c${stamp.shape}(${stamp.index})`, `color: ${stamp.color}; font-size:30px;`);
+        // console.log(stamp.index);
+        console.log(`%c${stamp.index}${stamp.secuence}${stamp.shape}`, `color: ${stamp.color}; font-size:15px;`);
+      })
   }
 
   /* arrow fuction can capture this.. but not normal function for example like this
@@ -52,17 +52,17 @@ class ScmObservable {
     let interval = setInterval(() => {
       // console.log(index);
       if (this.index <= this.limit) {
-
+        this.secuence += '- ';
         let st: Istamp = {
           color: this.color,
           shape: this.geometry,
-          index:this.index,
-          secuence:this.secuence
+          index: this.index,
+          secuence: this.secuence
         }
         observer.next(st);
         // `${this.color} : ${this.geometry} : ${this.index}`
         this.index += 1;
-        this.secuence +='- ';
+
       } else {
         observer.complete();
         clearInterval(interval);
@@ -80,12 +80,18 @@ var circleBlue$ = new ScmObservable('.', 'blue', 10, 1000);
 var triangleRed$ = new ScmObservable('^', 'red', 10, 1000);
 
 circleBlue$.getObservable()
-.map((stamp:Istamp)=> {
-  stamp.shape='map';
-  return stamp;
-})
-// .filter((stamp:Istamp)=> stamp.index%3===0)
-.subscribe(new MyObserver('info'))
+  .map((stamp:Istamp)=> {
+    stamp.shape='map';
+    return stamp;
+  })
+  // .filter((stamp:Istamp)=> stamp.index%3===0)
+  .scan((acc: Istamp, stam: Istamp) => {
+    acc.index = stam.index;
+    acc.secuence = stam.secuence;
+    acc.shape += stam.shape;
+    return acc
+  })
+  .subscribe(new MyObserver('info'))
 // triangleRed$.getObservable().subscribe(new MyObserver('error'))
 
 
